a38cbcce14e27ecc57b7fc3ebff7c2112b25a157
Oleksii Shevchuk <alxchk@gmail.com>
2019-10-24 23:05:26 +0300

    ccs-kerberos and winkerberos are incompatible, honor this

--- urllib_auth/auth.py
+++ urllib_auth/auth.py
@@ -9,19 +9,25 @@ from .ntlm import (
     create_NTLM_AUTHENTICATE_MESSAGE
 )
 
+has_gssapi_support = False
+has_sspi_ntlm_support = False
+has_sspi_nego_support = False
+
 try:
     import winkerberos as kerberos
+
     has_sspi_ntlm_support = True
     has_sspi_nego_support = True
+
     CONTINUE = kerberos.AUTH_GSS_CONTINUE
 except ImportError:
-    has_sspi_ntlm_support = False
+
     try:
         import kerberos
-        has_sspi_nego_support = True
+
+        has_gssapi_support = True
         CONTINUE = kerberos.AUTH_GSS_CONTINUE
     except ImportError:
-        has_sspi_nego_support = False
         CONTINUE = 1
 
 
@@ -50,35 +56,80 @@ class Authentication(object):
         self._ntlm_password = None
 
     def _create_auth1_message_sspi(
-            self, url, methods, user=None, password=None, domain=None, flags=0):
+            self, url, method, user=None,
+            password=None, domain=None, flags=0):
 
-        method = None
-        mechoid = None
+        method = method.upper()
         server = None
         principal = None
 
-        host = urlparse.urlparse(url).netloc
+        kwargs = {
+            'user': user,
+            'password': password,
+            'domain': domain,
+            'gssflags': flags
+        }
+
+        parsed = urlparse.urlparse(url)
+        host = parsed.netloc
+        scheme = parsed.scheme.upper()
         domain = host.rsplit(':', 1)[0]
 
-        if has_sspi_nego_support and METHOD_NEGOTIATE in methods:
-            mechoid = kerberos.GSS_MECH_OID_SPNEGO
-            method = METHOD_NEGOTIATE
-            server = 'HTTP@'+domain
-        elif has_sspi_ntlm_support and METHOD_NTLM in methods:
-            # This is only for SSPI
-            method = METHOD_NTLM
-            mechoid = kerberos.GSS_MECH_OID_NTLM
-            server = domain
+        if scheme.startswith('HTTP'):
+            service = 'HTTP'
         else:
-            raise AuthenticationError('No supported method')
+            service = scheme
+
+        use_gssapi = False
+
+        if has_sspi_nego_support and method == METHOD_NEGOTIATE.upper():
+            kwargs['mech_oid'] = kerberos.GSS_MECH_OID_SPNEGO
+            method = METHOD_NEGOTIATE
+            server = service + '@' + domain
+
+        elif has_sspi_ntlm_support and method == METHOD_NTLM.upper():
+            # This is only for SSPI
+            kwargs['mech_oid'] = kerberos.GSS_MECH_OID_NTLM
+            method = METHOD_NTLM
+            server = domain
+
+        elif has_gssapi_support and method == METHOD_NEGOTIATE.upper():
+            server = service + '@' + domain
+            kwargs['mech_oid'] = kerberos.GSS_MECH_OID_SPNEGO
+
+            if password:
+                raise NotImplementedError(
+                    "ccs-kerberos doesn't support password auth")
+
+            if user and domain:
+                principal = user + '@' + domain
+
+            del kwargs['user']
+            del kwargs['password']
+            del kwargs['domain']
+
+            use_gssapi = True
+
+        else:
+            raise NotImplementedError('No supported auth methods')
 
         result, self._ctx = kerberos.authGSSClientInit(
-            server, principal, flags, user, domain, password, mechoid
+            server, principal, **kwargs
         )
 
         if result < 0:
             self.logger.error('GSSAPI: authGSSClientInit Result: %d', result)
             raise AuthenticationError(result)
+
+        if use_gssapi:
+            result = kerberos.authGSSClientInquireCred(self._ctx)
+            if result < 0:
+                raise AuthenticationError(result)
+
+            self.logger.warning(
+                'GSSAPI: Using principal: %s',
+                kerberos.authGSSClientUserName(self._ctx)
+            )
 
         result = kerberos.authGSSClientStep(self._ctx, '')
         if result < 0:
@@ -98,17 +149,24 @@ class Authentication(object):
         return result, self._method, kerberos.authGSSClientResponse(self._ctx)
 
     def create_auth1_message(self, domain, user, pw, url, auth_methods):
-        if pw is None or METHOD_NEGOTIATE in auth_methods:
+        for method in auth_methods:
             try:
                 return self._create_auth1_message_sspi(
-                    url, auth_methods, user, pw, domain
-                )
-            except AuthenticationError as e:
-                self.logger.warning(
-                    'No login/password found for: URL: %s (%s)', url, e)
-                raise
+                    url, method, user, pw, domain,
 
-        if 'NTLM' not in auth_methods:
+                )
+
+            except NotImplementedError:
+                pass
+
+            except AuthenticationError as e:
+                self.logger.info('SSPI error: %s (ignore)', e)
+
+        if not user:
+            self.logger.warning(
+                'No login/password found for: URL: %s (%s)', url, e)
+
+        if 'NTLM' not in auth_methods or not user:
             raise AuthenticationError('No acceptable auth found for: URL: %s', url)
     
         self.logger.info('Fallback to py NTLM with creds: %s -> user=%s', url, user)
