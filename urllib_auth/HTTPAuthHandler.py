55931e98049590f00e05076145d4c691c264d60a
Oleksii Shevchuk <ol.shevchuk@samsung.com>
2019-10-25 16:33:45 +0300

    Fix Negotiate

--- urllib_auth/HTTPAuthHandler.py
+++ urllib_auth/HTTPAuthHandler.py
@@ -17,22 +17,72 @@ import httplib
 import socket
 import ntlm
 import re
+import ssl
 import logging
+
 
 from urllib import addinfourl
 
 from .auth import (
     Authentication, METHOD_NTLM, METHOD_NEGOTIATE,
-    CONTINUE, AuthenticationError
+    CONTINUE, COMPLETE, AuthenticationError,
+    get_supported_methods, kerberos
 )
+
+
+def infourl_to_sock(infourl):
+    return infourl.fp._sock.fp._sock
+
+
+def infourl_to_ssl_context(infourl):
+    sock = infourl_to_sock(infourl)
+    if not isinstance(sock, ssl.SSLSocket):
+        return None
+
+    return sock.context
+
+
+def infourl_to_ssl_certificate(infourl):
+    sock = infourl_to_sock(infourl)
+    if not isinstance(sock, ssl.SSLSocket):
+        return None
+
+    return sock.getpeercert(True)
+
+
+def make_infourl(response, req, fp=None):
+    def notimplemented():
+        raise NotImplementedError
+
+    response.readline = notimplemented
+    infourl = addinfourl(
+        fp or response, response.msg, req.get_full_url())
+    infourl.code = response.status
+    infourl.msg = response.reason
+    return infourl
+
+
+def consume_response_body(response):
+    content_length = response.getheader('content-length')
+    if content_length:
+        content_length = int(content_length)
+
+        # Finish previous response
+        response.begin()
+        response._safe_read(content_length)
+
+        # remove the reference to the socket, so that it can not be closed
+        # by the response object (we want to keep the socket open)
+        response.fp = None
+
 
 class AbstractAuthHandler(Authentication):
     __slots__ = (
         'passwd', 'add_password', '_debuglevel'
     )
 
-    auth_header = None
-    auth_header_field = None
+    auth_header_request = None
+    auth_header_response = None
 
     def __init__(self, password_mgr=None, debuglevel=0):
         """Initialize an instance of a AbstractAuthHandler.
@@ -60,23 +110,22 @@ Verify "normal" operation.
 
     def set_http_debuglevel(self, level):
         self._debuglevel = level
-    
-    def http_error_authentication_required(self, req, fp, headers):
-        methods = set()
-        unsupported = set()
-        url = req.get_full_url()
 
-        for header in headers.getallmatchingheaders(self.auth_header_field):
+    def get_authentication_methods(self, headers):
+        methods = set()
+
+        for header in headers.getallmatchingheaders(self.auth_header_response):
             _, value = header.split(':', 1)
             values = tuple(x.strip().lower() for x in value.split(','))
             for value in values:
-                if value == 'ntlm':
-                    methods.add(METHOD_NTLM)
-                elif value == 'negotiate':
-                    methods.add(METHOD_NEGOTIATE)
-                else:
-                    unsupported.add(value)
-        
+                methods.add(value)
+
+        return get_supported_methods(methods)
+
+    def http_error_authentication_required(self, req, infourl, headers):
+        url = req.get_full_url()
+
+        methods, unsupported = self.get_authentication_methods(headers)
         if not methods:
             self.logger.warning(
                 'No supported auth method: URL=%s, methods=%s',
@@ -88,10 +137,10 @@ Verify "normal" operation.
             url, ','.join(methods), ','.join(unsupported)
         )
 
-        fp.close()
-        return self.retry_using_http_auth(req, methods, headers)
+        return self.retry_using_http_auth(req, infourl, methods, headers)
 
-    def retry_using_http_auth(self, req, auth_methods, headers):
+    def retry_using_http_auth(self, req, infourl, auth_methods, headers):
+        connection = headers.get('connection')
 
         # ntlm secures a socket, so we must use the same socket for the complete handshake
         headers = dict(req.headers)
@@ -107,9 +156,11 @@ Verify "normal" operation.
         if user is not None and '\\' in user:
             domain, user = user.split('\\', 1)
 
+        certificate = infourl_to_ssl_certificate(infourl)
+
         try:
             result, method, payload = self.create_auth1_message(
-                domain, user, pw, url, auth_methods)
+                domain, user, pw, url, auth_methods, certificate)
             if result != CONTINUE:
                 self.logger.error('Something went wrong? %s', result)
                 return None
@@ -117,17 +168,44 @@ Verify "normal" operation.
         except AuthenticationError:
             self.logger.warning('No way to perform authentication: URL=%s', url)
             return None
-    
-        headers[self.auth_header] = ' '.join([method, payload])
+
+        self.logger.debug(
+            'Selected auth method=%s payload=%s', method, payload
+        )
+
+        headers.update({
+            self.auth_header_request: ' '.join([method, payload]),
+            'Connection': 'keep-alive'
+        })
 
         h = None
+
         if url.startswith('https://'):
-            h = httplib.HTTPSConnection(host)
+            try:
+                old_ssl_context = infourl_to_ssl_context(infourl)
+                self.logger.debug('Reuse SSL Context: %s', old_ssl_context)
+            except Exception as e:
+                self.logger.exception('SSL Context not found')
+                old_ssl_context = None
+
+            h = httplib.HTTPSConnection(host, context=old_ssl_context)
         else:
             h = httplib.HTTPConnection(host)
 
-        h.set_debuglevel(self._debuglevel)
-    
+        if self._debuglevel or self.logger.getEffectiveLevel() == logging.DEBUG:
+            h.set_debuglevel(1)
+
+        if connection and connection.lower() == 'close':
+            self.logger.debug(
+                'New connection required: host=%s', host
+            )
+
+            infourl.close()
+            infourl = None
+        else:
+            self.logger.debug('Reuse connection %s')
+            h.sock = infourl_to_sock(infourl)
+
         # We must keep the connection because NTLM authenticates the
         # connection, not single requests
 
@@ -135,83 +213,88 @@ Verify "normal" operation.
             (name.title(), val)
             for name, val in headers.items()
         )
-        headers['Connection'] = 'Keep-Alive'
-    
-        h.request(req.get_method(), req.get_selector(), req.data, headers)
-        r = h.getresponse()
-        r.begin()
-        r._safe_read(int(r.getheader('content-length')))
 
-        if r.getheader('set-cookie'):
-            # this is important for some web applications that store authentication-related
-            # info in cookies (it took a long time to figure out)
-            headers['Cookie'] = r.getheader('set-cookie')
+        self.logger.debug('Send request, headers=%s', headers)
 
-        # remove the reference to the socket, so that it can not be closed
-        # by the response object (we want to keep the socket open)
-        r.fp = None
-        
-        # some Exchange servers send two WWW-Authenticate headers, one with the NTLM challenge
-        # and another with the 'Negotiate' keyword - make sure we operate on the right one
         payload = None
 
-        for header, value in r.getheaders():
-            if header.lower() != self.auth_header_field.lower():
+        h.request(req.get_method(), req.get_selector(), req.data, headers)
+        response = h.getresponse()
+
+        if response.getheader('set-cookie'):
+            # this is important for some web applications that store authentication-related
+            # info in cookies (it took a long time to figure out)
+            headers['Cookie'] = response.getheader('set-cookie')
+
+        # some Exchange servers send two WWW-Authenticate headers, one with the NTLM challenge
+        # and another with the 'Negotiate' keyword - make sure we operate on the right one
+
+        for header, value in response.getheaders():
+            if header.lower() != self.auth_header_response.lower():
+                self.logger.debug('Not matched header: %s = %s', header, value)
                 continue
 
             match = re.match(
-                ' '.join([method, r'([A-Za-z0-9+\-/=]+)']), value
-            )
+                r'^(?:{}\s+)?([A-Za-z0-9+\-/=]+)$'.format(method), value)
             if not match:
+                self.logger.debug('Not matched value: %s = %s', header, value)
                 continue
-        
+
             payload, = match.groups()
-        
+
         if not payload:
-            self.logger.error('Auth header response not found, URL=%s', url)
+            self.logger.error(
+                'Auth header response not found, Status=%s URL=%s',
+                response.status, url)
+
             return None
 
-        _, method, payload = self.create_auth2_message(payload)
-
-        headers[self.auth_header] = ' '.join([method, payload])
-        headers['Connection'] = 'Close'
-        headers = dict((name.title(), val)
-                        for name, val in headers.items())
+        self.logger.debug('Step2: Method: %s, Payload: %s', method, payload)
 
         try:
-            h.request(req.get_method(), req.get_selector(),
-                        req.data, headers)
-            # none of the configured handlers are triggered, for example
-            # redirect-responses are not handled!
-            response = h.getresponse()
+            status, method, payload = self.create_auth2_message(payload)
+        except AuthenticationError as e:
+            self.logger.error('Step2: Authentication failed (%s)', e)
+            return None
 
-            def notimplemented():
-                raise NotImplementedError
+        if status == CONTINUE:
+            self.logger.debug(
+                'Step2: Method: %s, Response Payload: %s', method, payload)
+            headers[self.auth_header_request] = ' '.join([method, payload])
 
-            response.readline = notimplemented
-            infourl = addinfourl(
-                response, response.msg, req.get_full_url())
-            infourl.code = response.status
-            infourl.msg = response.reason
+            try:
+                consume_response_body(response)
 
-            if infourl.code in (401, 407):
-                self.logger.warning(
-                    'Authentication failed: URL=%s, CODE=%s',
-                    req.get_full_url(), infourl.code
-                )
-            else:
-                self.logger.info('Authentication OK: URL=%s', req.get_full_url())
+                h.request(req.get_method(), req.get_selector(),
+                            req.data, headers)
+                # none of the configured handlers are triggered, for example
+                # redirect-responses are not handled!
+                response = h.getresponse()
 
-            return infourl
+            except socket.error as err:
+                self.logger.exception('')
+                raise urllib2.URLError(err)
 
-        except socket.error, err:
-            raise urllib2.URLError(err)
+        else:
+            self.logger.debug(
+                'Step2: Method: %s, Continuation not required (%d)', method, status)
+
+        infourl = make_infourl(response, req)
+        if infourl.code in (401, 407):
+            self.logger.warning(
+                'Authentication failed: URL=%s, CODE=%s',
+                req.get_full_url(), infourl.code
+            )
+        else:
+            self.logger.info('Authentication OK: URL=%s', req.get_full_url())
+
+        return infourl
 
 
 class HTTPAuthHandler(AbstractAuthHandler, urllib2.BaseHandler):
 
-    auth_header = 'Authorization'
-    auth_header_field = 'www-authenticate'
+    auth_header_request = 'Authorization'
+    auth_header_response = 'www-authenticate'
 
     def http_error_401(self, req, fp, code, msg, headers):
         try:
@@ -226,8 +309,8 @@ class ProxyAuthHandler(AbstractAuthHandler, urllib2.BaseHandler):
         CAUTION: this class has NOT been tested at all!!!
         use at your own risk
     """
-    auth_header = 'Proxy-authorization'
-    auth_header_field = 'proxy-authenticate'
+    auth_header_request = 'Proxy-authorization'
+    auth_header_response = 'proxy-authenticate'
 
     def http_error_407(self, req, fp, code, msg, headers):
         try:
--- urllib_auth/auth.py
+++ urllib_auth/auth.py
@@ -1,8 +1,9 @@
 import urlparse
+import hashlib
 
 from .ntlm import (
     NTLM_TYPE1_FLAGS,
-    
+
     NTLM_NegotiateOemDomainSupplied,
     create_NTLM_NEGOTIATE_MESSAGE,
     parse_NTLM_CHALLENGE_MESSAGE,
@@ -13,6 +14,9 @@ has_gssapi_support = False
 has_sspi_ntlm_support = False
 has_sspi_nego_support = False
 
+CONTINUE = 0
+COMPLETE = 1
+
 try:
     import winkerberos as kerberos
 
@@ -20,19 +24,47 @@ try:
     has_sspi_nego_support = True
 
     CONTINUE = kerberos.AUTH_GSS_CONTINUE
-except ImportError:
+    COMPLETE = kerberos.AUTH_GSS_COMPLETE
 
+except ImportError:
     try:
         import kerberos
 
         has_gssapi_support = True
-        CONTINUE = kerberos.AUTH_GSS_CONTINUE
-    except ImportError:
-        CONTINUE = 1
 
+        CONTINUE = kerberos.AUTH_GSS_CONTINUE
+        COMPLETE = kerberos.AUTH_GSS_COMPLETE
+
+    except ImportError:
+        kerberos = None
 
 METHOD_NTLM = 'NTLM'
 METHOD_NEGOTIATE = 'Negotiate'
+METHOD_KERBEROS = 'Kerberos'
+
+METHODS_PRIORITY = (
+    METHOD_NEGOTIATE, METHOD_NTLM, METHOD_KERBEROS
+)
+
+
+def _get_method(method):
+    for known_method in (METHOD_NTLM, METHOD_NEGOTIATE):
+        if method.lower() == known_method.lower():
+            return known_method
+
+
+def get_supported_methods(methods):
+    methods = set(
+        method for method in [
+            _get_method(method) for method in methods
+        ] if method is not None
+    )
+
+    return [
+        method for method in METHODS_PRIORITY if method in methods
+    ], [
+        method for method in methods if method not in METHODS_PRIORITY
+    ]
 
 
 class AuthenticationError(Exception):
@@ -44,6 +76,8 @@ class Authentication(object):
         'logger',
         '_ctx', '_method',
 
+        '_channel_bindings',
+
         '_ntlm_user', '_ntlm_domain', '_ntlm_password',
     )
 
@@ -54,12 +88,13 @@ class Authentication(object):
         self._ntlm_user = None
         self._ntlm_domain = None
         self._ntlm_password = None
+        self._channel_bindings = None
 
     def _create_auth1_message_sspi(
             self, url, method, user=None,
-            password=None, domain=None, flags=0):
+            password=None, domain=None, flags=0,
+            certificate=None):
 
-        method = method.upper()
         server = None
         principal = None
 
@@ -73,42 +108,55 @@ class Authentication(object):
         parsed = urlparse.urlparse(url)
         host = parsed.netloc
         scheme = parsed.scheme.upper()
-        domain = host.rsplit(':', 1)[0]
+        hostname = host.rsplit(':', 1)[0]
 
         if scheme.startswith('HTTP'):
             service = 'HTTP'
         else:
             service = scheme
 
-        use_gssapi = False
+        need_inquire_cred = False
 
-        if has_sspi_nego_support and method == METHOD_NEGOTIATE.upper():
-            kwargs['mech_oid'] = kerberos.GSS_MECH_OID_SPNEGO
-            method = METHOD_NEGOTIATE
-            server = service + '@' + domain
+        if (has_sspi_nego_support or has_gssapi_support) and method in (
+                METHOD_NEGOTIATE, METHOD_KERBEROS):
 
-        elif has_sspi_ntlm_support and method == METHOD_NTLM.upper():
+            if certificate:
+                self.set_certificate(certificate)
+
+            if method == METHOD_NEGOTIATE:
+                mech_oid = kerberos.GSS_MECH_OID_SPNEGO
+            else:
+                mech_oid = kerberos.GSS_MECH_OID_KRB5
+
+            flags |= \
+                kerberos.GSS_C_MUTUAL_FLAG | \
+                kerberos.GSS_C_SEQUENCE_FLAG
+
+            kwargs.update({
+                'mech_oid': mech_oid,
+                'gssflags': flags
+            })
+
+            server = service + '@' + hostname
+
+            if has_gssapi_support:
+                del kwargs['user']
+                del kwargs['password']
+                del kwargs['domain']
+
+                need_inquire_cred = True
+
+                if password:
+                    raise NotImplementedError(
+                        "ccs-kerberos doesn't support password auth")
+
+            if user:
+                principal = user + '@' + hostname
+
+        elif has_sspi_ntlm_support and method == METHOD_NTLM:
             # This is only for SSPI
             kwargs['mech_oid'] = kerberos.GSS_MECH_OID_NTLM
-            method = METHOD_NTLM
-            server = domain
-
-        elif has_gssapi_support and method == METHOD_NEGOTIATE.upper():
-            server = service + '@' + domain
-            kwargs['mech_oid'] = kerberos.GSS_MECH_OID_SPNEGO
-
-            if password:
-                raise NotImplementedError(
-                    "ccs-kerberos doesn't support password auth")
-
-            if user and domain:
-                principal = user + '@' + domain
-
-            del kwargs['user']
-            del kwargs['password']
-            del kwargs['domain']
-
-            use_gssapi = True
+            server = hostname
 
         else:
             raise NotImplementedError('No supported auth methods')
@@ -121,7 +169,12 @@ class Authentication(object):
             self.logger.error('GSSAPI: authGSSClientInit Result: %d', result)
             raise AuthenticationError(result)
 
-        if use_gssapi:
+        self.logger.debug(
+            'GSSAPI: New context for SPN=%s%s', server,
+            '' if not principal else 'Principal='+principal
+        )
+
+        if need_inquire_cred:
             result = kerberos.authGSSClientInquireCred(self._ctx)
             if result < 0:
                 raise AuthenticationError(result)
@@ -131,7 +184,14 @@ class Authentication(object):
                 kerberos.authGSSClientUserName(self._ctx)
             )
 
-        result = kerberos.authGSSClientStep(self._ctx, '')
+        kwargs = {}
+        if self._channel_bindings:
+            kwargs['channel_bindings'] = self._channel_bindings
+
+        result = kerberos.authGSSClientStep(
+            self._ctx, '', **kwargs
+        )
+
         if result < 0:
             self.logger.error('GSSAPI: authGSSClientInit Result: %d', result)
             raise AuthenticationError(result)
@@ -140,35 +200,73 @@ class Authentication(object):
         return result, method, kerberos.authGSSClientResponse(self._ctx)
 
     def _create_auth2_message_sspi(self, payload):
-        result = kerberos.authGSSClientStep(self._ctx, payload)
+        kwargs = {}
+        if self._channel_bindings:
+            kwargs['channel_bindings'] = self._channel_bindings
+
+        result = kerberos.authGSSClientStep(
+            self._ctx, payload, **kwargs
+        )
+
+        payload = kerberos.authGSSClientResponse(self._ctx)
 
         if result < 0:
-            self.logger.error('GSSAPI: authGSSClientStep (2) Result: %d', result)
+            self.logger.error('GSSAPI: authGSSClientStep (2) Step Result: %d', result)
             raise AuthenticationError(result)
-        
-        return result, self._method, kerberos.authGSSClientResponse(self._ctx)
+        elif result == COMPLETE:
+            if payload is not None:
+                if self._method != METHOD_NTLM:
+                    # Known bug for NTLM and winkerberos
+                    self.logger.warning(
+                        'GSSAPI: API bug: payload provided but step is not required')
+                result = CONTINUE
+            else:
+                self.logger.info(
+                    'GSSAPI: authGSSClientStep (2) not required for %s',
+                    self._method)
 
-    def create_auth1_message(self, domain, user, pw, url, auth_methods):
-        for method in auth_methods:
+        return result, self._method, payload
+
+    def set_certificate(self, certificate):
+        # FIXME: Handle case with different hash function somehow..
+
+        application_data = b'tls-server-end-point:' + hashlib.sha256(
+            certificate).digest()
+
+        self._channel_bindings = kerberos.channelBindings(
+            application_data=application_data)
+
+        self.logger.debug(
+            'Set channel bindings: %s -> %s',
+            repr(application_data), self._channel_bindings
+        )
+
+    def create_auth1_message(self, domain, user, pw, url, auth_methods, certificate):
+        supported_auth_methods, _ = get_supported_methods(auth_methods)
+        for method in supported_auth_methods:
             try:
                 return self._create_auth1_message_sspi(
                     url, method, user, pw, domain,
-
+                    certificate=certificate
                 )
 
             except NotImplementedError:
-                pass
+                self.logger.debug(
+                    'Not supported conditions for method %s', method)
 
             except AuthenticationError as e:
-                self.logger.info('SSPI error: %s (ignore)', e)
+                self.logger.info(
+                    'SSPI error: method=%s error=%s (ignore)', method, e)
 
-        if not user:
-            self.logger.warning(
-                'No login/password found for: URL: %s (%s)', url, e)
+        self.logger.debug(
+            'No usable SSPI/GSSAPI auth method found for URL=%s', url)
 
-        if 'NTLM' not in auth_methods or not user:
+        if user is None:
+            self.logger.info('No credentials found for URL=%s', url)
+
+        if METHOD_NTLM not in auth_methods or not user:
             raise AuthenticationError('No acceptable auth found for: URL: %s', url)
-    
+
         self.logger.info('Fallback to py NTLM with creds: %s -> user=%s', url, user)
 
         type1_flags = NTLM_TYPE1_FLAGS
@@ -186,13 +284,13 @@ class Authentication(object):
 
     def create_auth2_message(self, payload):
         if self._ctx:
-            return self._create_auth2_message_sspi(payload)    
-        
+            return self._create_auth2_message_sspi(payload)
+
         if self._method != METHOD_NTLM:
             raise AuthenticationError('Invalid state (expected method NTLM)')
-        
+
         ServerChallenge, NegotiateFlags = parse_NTLM_CHALLENGE_MESSAGE(payload)
-        return 0, self._method, create_NTLM_AUTHENTICATE_MESSAGE(
+        return CONTINUE, self._method, create_NTLM_AUTHENTICATE_MESSAGE(
             ServerChallenge,
             self._ntlm_user, self._ntlm_domain, self._ntlm_password,
             NegotiateFlags
